8f16f7f355c012366656aefe5487dea0e8faac24
Meghan <meghan.landry@sjsu.edu>
2018-11-02 14:09:56 -0700

    Update to portfolio

--- README.md
+++
@@ -1,5 +0,0 @@
-# Meghan Landry
-
-### I am an artist. This is my portfolio site.
-
-This is a *README* file for my repository. It is written using **Markdown**.
---
+++ art75_OLD/in-class_exercise_1/sketch.js
@@ -0,0 +1,43 @@
+// declare variables here
+let intMove = 0;
+
+function setup() {
+  // put setup code here --> this runs once upon launch
+
+  createCanvas(500, 500);
+  background(0);
+
+
+
+}
+
+function draw() {
+ //
+
+}
+console.console.log("Hi, okay.");
+
+
+
+function draw() {
+
+  // put drawing code here --> this loops every frame
+  // set background color in grayscale:
+ background(150);
+
+ // draw rectangle
+ intMove++
+
+ fill(255, 255, 255, 80)
+ rectMode(CENTER)
+ rect(intMove, 200, 80, 80)
+
+ // draw ellipse
+ fill(255, 200, 200, 80)
+ ellipse(200 + 20, 200, 150, 80)
+
+
+}
+
+
+// write custom functions here
