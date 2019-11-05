89230bda835ffcf6fa61d0d6ea6ad24318518d88
Meghan <meghan.landry@sjsu.edu>
2019-11-04 18:39:10 -0800

    c

--- art101/clock/clock.js
+++ art101/clock/clock.js
@@ -1,7 +1,4 @@
-let cloud_1;
-let cloud_2;
-let cloud_3;
-let cloud_4;
+clouds = [];
 
 //why are libraries such a pain
 let sec = 0;
@@ -14,62 +11,79 @@ let skyNight;
 
 //accounting for AM and PM in loop
 let ampmCheck;
+let mapDeg;
+let mapRay;
+let hrCt;
+let label;
+let setUpCheck = true;
 
 //arrays of objects
 stars = [];
-clouds = [];
 rays = [];
+cloudList = [];
+
 
 function preload() {
-clouds[0] = loadImage('cloud_1.png');
-clouds[1] = loadImage('cloud_2.png');
-clouds[2] = loadImage('cloud_3.png');
-clouds[3] = loadImage('cloud_4.png');
+  clouds[0] = loadImage('cloud_1.png');
+  clouds[1] = loadImage('cloud_2.png');
+  clouds[2] = loadImage('cloud_3.png');
+  clouds[3] = loadImage('cloud_4.png');
 }
+
 
 //setup
 function setup() {
-  console.log(clouds.length); //?????????????????????
 
   background(0);
   createCanvas(windowWidth, windowHeight);
   noStroke();
+  imageMode(CENTER);
+  angleMode(DEGREES);
+
 
   sec = second();
   min = minute();
   hr = hour();
   ampmChecker();
-  let totMinPM = (((hr+ampmCheck) * 60) + min);
-  let hrCt = (hr-6);
-
+  let totMinPM = (((hr + ampmCheck) * 60) + min);
+  hrCt = (hr - 6);
+  mapDeg = map(min, 0, 60, 0, windowWidth) + sec;
+  mapRay = map(min, 0, 60, 0, 360);
+  //console.log("a: " + mapDeg + " " + min + " / " + windowWidth);
   skyDay = new Sun();
   skyNight = new Moon();
 
-  if ((hr <= 6) && (hr >= 18)){
-  for (let i = 0; i < totMinPM; i++) {
-    stars[i] = new Stars();
-    stars[i].display();
-  }
+  if ((hr <= 6) && (hr >= 18)) {
+    for (let i = 0; i < totMinPM; i++) {
+      stars[i] = new Stars();
+      stars[i].display();
+    }
   } else {
     for (let i = 0; i < hrCt; i++) {
-      console.log("running");
-      var r = floor(random(0, clouds.length));
-      var n = new Clouds(100, 100, clouds[r]);
-      clouds.push(n);
+      label = (i + 1);
+      cloudList[i] = new Cloud(label);
+      cloudList[i].move();
+      //cloudList[i].display();
+      //console.log("b: " + cloudList.length + " , " + label);
+    }
   }
-  }
+  setUpCheck = false;
+  circletest();
 }
 
 //loop
 function draw() {
-  console.log(clouds.length); //?????????????????????????
+
   //timekeeping
   sec = second();
   min = minute();
   hr = hour();
   ampmChecker();
-  hrCt = (hr-6);
-  totMinPM = (((hr+ampmCheck) * 60) + min)
+  hrCt = (hr - 6);
+  totMinPM = (((hr + ampmCheck) * 60) + min)
+  mapDeg = map(min, 0, 60, 0, windowWidth) + sec;
+  mapRay = map(min, 0, 60, 0, 360);
+  //console.log("a: " + min + " " + mapDeg + " / " + windowWidth);
   //console.log(totMinPM + " . " + min + " . " + hr );
 
   //day loop
@@ -77,40 +91,63 @@ function draw() {
     background(243, 225, 225, 255)
     stars = [];
     skyDay.display();
-  } else if ((hr == 18) && (min == 0) && (second < 15)){
+  } else if ((hr == 18) && (min == 0) && (second < 15)) {
+    clouds = [];
     background(20, 20, 45, 255)
-  }else if((hr >= 6) && (hr <= 17)) {
-    background(243, 225, 225, 10);
+  } else if ((hr >= 6) && (hr <= 17)) {
+    background(243, 225, 225, 255);
     skyDay.expand();
     skyDay.display();
-    for (let i = 0; i < clouds.length; i++) {
-      var r = floor(random(0, clouds.length));
-      var n = new Clouds(100, 100, clouds[r]);
-      clouds[r].display();
-      //clouds[n].push();
-      //clouds[i] = new Clouds();
-  }
-  //night loop
-} else {
+    //clouds
+    for (let i = 0; i < hrCt; i++) {
+      if (hrCt > (cloudList.length)) {
+        label++;
+        cloudList[i].move();
+        cloudList.push(new Cloud(label));
+      }
+    }
+    for (let i = 0; i < cloudList.length; i++) {
+      cloudList[i].move();
+      cloudList[i].display();
+    }
+  } else {
     background(20, 20, 45)
     if (totMinPM > (stars.length)) {
       stars.push(new Stars());
     }
     for (let i = 0; i < stars.length; i++) {
-    stars[i].display();
+      stars[i].display();
     }
     skyNight.display();
   }
+  circletest();
 }
 
 //FUNCTIONS
 //sets up AM/PM variable
 function ampmChecker() {
-  if (hr<12) {
+  if (hr < 12) {
     ampmCheck = 6;
   } else {
     ampmCheck = -18;
   }
+}
+
+function circletest() {
+  console.log("circle running");
+  circles = 13;
+  angle = 360 / circles;
+
+for(var i = 0; i < circles; i++){
+
+    xCircle = windowWidth/2 + cos(angle*i) * 130;
+    yCircle = windowHeight/2 + sin(angle*i) * 130;
+    xCircle2 = windowWidth/2 + cos(angle*i) * (130+10*i);
+    yCircle2 = windowHeight/2 + sin(angle*i) * (130+10*i);
+    strokeWeight(10);
+    stroke(255, 25);
+    line(xCircle, yCircle, xCircle2, yCircle2);
+}
 }
 
 
@@ -118,24 +155,30 @@ function ampmChecker() {
 //sun
 class Sun {
   constructor() {
+    noStroke();
     this.x = (windowWidth / 2);
     this.y = (windowHeight / 2);
-    this.diameter = 500;
+    this.r = 200;
   }
 
   expand() {
-    this.diameter += (random(-3, 3));
+    this.r += (random(-3, 3));
   }
 
   display() {
+    noStroke();
     fill(255, 255, 255, 255)
-    ellipse(this.x, this.y, this.diameter, this.diameter);
+    ellipse(this.x, this.y, this.r*2);
+    fill(255, 255, 255, 50)
+    ellipse(this.x, this.y, (this.r*2) + random(50));
+    ellipse(this.x, this.y, (this.r*2) + random(100));
   }
 }
 
 //moon
 class Moon {
   constructor() {
+    noStroke();
     this.x = (windowWidth / 2);
     this.y = (windowHeight / 2);
     this.r = 100;
@@ -146,51 +189,81 @@ class Moon {
   }
 
   display() {
+    noStroke();
     fill(255, 225, 225, 255)
-    ellipse(this.x, this.y, this.r*2);
+    ellipse(this.x, this.y, this.r * 2);
     fill(255, 255, 255, 50)
-    ellipse(this.x, this.y, this.r*2 + 15 + (random(-3, 3)));
+    ellipse(this.x, this.y, this.r * 2 + 15 + (random(-3, 3)));
   }
 }
 
 //stars
 class Stars {
   constructor() {
+    noStroke();
     this.x = random(windowWidth);
     this.y = random(windowHeight);
     this.r = random(3);
   }
 
   display() {
+    noStroke();
     fill(255, 255, 255);
-    ellipse(this.x, this.y, this.r*2);
+    ellipse(this.x, this.y, this.r * 2);
     fill(255, 255, 255, 75);
-    ellipse(this.x, this.y, this.r*2 + 1 + (random(-3, 3)));
+    ellipse(this.x, this.y, this.r * 2 + 1 + (random(-3, 3)));
   }
 }
 
 //clouds
 
-class Clouds {
-  contructor (x, y, img){
-    this.x = 100;
-    this.y = 100;
-    this.img = img;
-    // this.tintEffect = random(100);
-    // this.speed = 1;
-    // this.endpoint = endpoint;
+class Cloud {
+  constructor(label) {
+    this.x = mapDeg;
+    this.y = (windowWidth * (random(.5, .9)));
+    this.tint = random(230, 255);
+    this.type = floor(random(4));
+    this.endPoint = false;
+    this.label = label;
+    this.stop = (windowWidth - (this.label*(windowWidth/12)));
   }
 
-    display(){
-      image(this.x, this.y, this.img);
+  move() {
+    if ((hrCt > this.label) && (setUpCheck == true)) {
+      this.endPoint = true;
+    }
+
+    if (this.endPoint == false) {
+      if (this.x < this.stop) {
+        this.x = mapDeg;
+      } else {
+        this.endPoint = true;
+        this.x = this.stop;
+      }
+    } else {
+      this.x = this.stop;
     }
   }
 
+  display() {
+    tint(this.tint);
+    image(clouds[this.type], this.x, this.y, width / 2, height / 3);
+  }
+}
+
 class Shine {
-  cunstructor(){
+  constructor() {
     this.x;
     this.y;
     this.len;
+    strokeWeight(5);
+  }
+
+  display() {
+    line(this.x, this.y)
+  }
+
+  grow(){
 
   }
 }
