d79ff798059787a8aec0a13b58e0150980d3d223
meghanlandry <42850294+meghanlandry@users.noreply.github.com>
2020-11-11 18:58:41 -0800

    animation after load

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 //Some of code from here: https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
 // When the user scrolls the page, execute myFunction
+
 window.onload = runScript;
 
-function runScript(){
+
+
+
+// makes nav stick to top of page, swaps to animated bg after rest of page has loaded
+function runScript() {
   window.onscroll = function() {
-    stickyNav()
+    stickyNav();
   };
 
   var navbar = document.getElementById("navbar");
   var sticky = navbar.offsetTop;
 
   // Used understanding of above example to make a expand button
+  var landing = document.getElementById("landing");
+  changeImage();
   var text5 = document.getElementById("text5");
   var text2 = document.getElementById("text2");
   var text12 = document.getElementById("text12");
@@ -18,7 +25,13 @@ function runScript(){
   var text3 = document.getElementById("text3");
   var text4 = document.getElementById("text4");
   var gallery = document.getElementById("gallery");
-  var more = document.getElementById("more"); document.getElementById("more").addEventListener("click", expand);
+  var more = document.getElementById("more");
+  document.getElementById("more").addEventListener("click", expand);
+
+  function changeImage() {
+    landing.classList.add("landing");
+    landing.classList.remove("preload");
+  }
 
   function stickyNav() {
     if (window.pageYOffset >= sticky) {
@@ -26,6 +39,11 @@ function runScript(){
     } else {
       navbar.classList.remove("sticky");
     }
+  }
+
+  function changeImage() {
+    landing.classList.add("landing");
+    landing.classList.remove("preload");
   }
 
   function expand() {
