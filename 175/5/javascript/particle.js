f0211adde8f436e42214850646aab1ce056e3763
meghanlandry <42850294+meghanlandry@users.noreply.github.com>
2020-12-10 13:36:05 -0800

    comments

--- 175/5/javascript/particle.js
+++ 175/5/javascript/particle.js
@@ -1,11 +1,15 @@
+//this file constructs particles
+
 var fade = 10;
 
+//sets up particles
 function Particle() {
   this.pos = createVector(random(width), random(height));
   this.vel = createVector(0, 0);
   this.acc = createVector(0, 0);
-  this.maxspeed = 5;
+  this.maxspeed = 5; //how fast the lines travel, more likely to be gated by framerate
 
+  //makes lines continuous
   this.prevPos = this.pos.copy();
 
   this.update = function() {
@@ -15,6 +19,7 @@ function Particle() {
     this.acc.mult(0);
   }
 
+  //makes particles follow nearest invisible particle
   this.follow = function(vectors) {
     var x = floor(this.pos.x / scl);
     var y = floor(this.pos.y / scl);
@@ -23,12 +28,16 @@ function Particle() {
     this.applyForce(force);
   }
 
+  //I wish I undestood vector particles well enough to understand what this does.
   this.applyForce = function(force) {
     this.acc.add(force);
   }
+
+  //displays the lines
   this.show = function() {
     this.fadeOut();
 
+    //draws lines to render object to be displayed from main script
     spaceBase.stroke(255, 200, 200, floor(fade));
     spaceBase.strokeWeight(1);
 
@@ -36,11 +45,13 @@ function Particle() {
     this.updatePrev();
   }
 
+  //position
   this.updatePrev = function() {
     this.prevPos.x = this.pos.x;
     this.prevPos.y = this.pos.y;
   }
 
+  //prevents particles from drawing actual lines
   this.edges = function() {
     if (this.pos.x > width) {
       this.pos.x = 0;
@@ -60,6 +71,7 @@ function Particle() {
     }
   }
 
+//function for fading lines out
   this.fadeOut = function() {
     if (fade < 5) {
       fade = 10;
