e9270ccb85191590595e6c93f6847e680dc72f02
meghanlandry <42850294+meghanlandry@users.noreply.github.com>
2022-02-08 12:32:49 -0800

    site overhaul

---
+++ old site/script.js
@@ -0,0 +1,50 @@
+//Some of code from here: https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
+// When the user scrolls the page, execute myFunction
+
+window.onload = runScript;
+
+
+
+// makes nav stick to top of page, swaps to animated bg after rest of page has loaded
+function runScript() {
+
+  var navbar = document.getElementById("navbar");
+  var sticky = navbar.offsetTop;
+  window.onscroll = function() {
+    stickyNav();
+  };
+
+  // Used understanding of above example to make a expand button
+  var body = document.getElementById("mainpage");
+  var landing = document.getElementById("landing");
+  var text5 = document.getElementById("text5");
+  var text2 = document.getElementById("text2");
+  var text12 = document.getElementById("text12");
+  var text6 = document.getElementById("text6");
+  var text3 = document.getElementById("text3");
+  var text4 = document.getElementById("text4");
+  var gallery = document.getElementById("gallery");
+  var more = document.getElementById("more");
+  document.getElementById("more").addEventListener("click", expand);
+
+
+  function stickyNav() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+
+  function expand() {
+    gallery.classList.add("artexpand");
+    gallery.classList.remove("art");
+    more.classList.add("hide");
+    text5.classList.remove("hide");
+    text2.classList.remove("hide");
+    text12.classList.remove("hide");
+    text6.classList.remove("hide");
+    text3.classList.remove("hide");
+    text4.classList.remove("hide");
+  }
+}
--- script.js
+++ script.js
@@ -1,32 +1,21 @@
-//Some of code from here: https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
-// When the user scrolls the page, execute myFunction
-
 window.onload = runScript;
 
-
-
-// makes nav stick to top of page, swaps to animated bg after rest of page has loaded
 function runScript() {
+  var close = document.getElementById("close");
+  var modalsCollection = document.getElementsByClassName("modal");
+  var thumbsCollection = document.getElementsByClassName("flexItem");
+  var charCollection = document.getElementsByClassName("char");
+  var modals = Array.from(modalsCollection);
+  var thumbs = Array.from(thumbsCollection);
+  var chars = Array.from(charCollection);
 
   var navbar = document.getElementById("navbar");
   var sticky = navbar.offsetTop;
+
+  // NavBar
   window.onscroll = function() {
     stickyNav();
   };
-
-  // Used understanding of above example to make a expand button
-  var body = document.getElementById("mainpage");
-  var landing = document.getElementById("landing");
-  var text5 = document.getElementById("text5");
-  var text2 = document.getElementById("text2");
-  var text12 = document.getElementById("text12");
-  var text6 = document.getElementById("text6");
-  var text3 = document.getElementById("text3");
-  var text4 = document.getElementById("text4");
-  var gallery = document.getElementById("gallery");
-  var more = document.getElementById("more");
-  document.getElementById("more").addEventListener("click", expand);
-
 
   function stickyNav() {
     if (window.pageYOffset >= sticky) {
@@ -36,15 +25,69 @@ function runScript() {
     }
   }
 
-  function expand() {
-    gallery.classList.add("artexpand");
-    gallery.classList.remove("art");
-    more.classList.add("hide");
-    text5.classList.remove("hide");
-    text2.classList.remove("hide");
-    text12.classList.remove("hide");
-    text6.classList.remove("hide");
-    text3.classList.remove("hide");
-    text4.classList.remove("hide");
+// modals
+  for (i = 0; i < thumbs.length; i++) {
+    thumbs[i].addEventListener("click", select);
+    thumbs[i].addEventListener("mouseover", playPreview);
+    thumbs[i].addEventListener("mouseout", pausePreview);
   }
+
+  close.addEventListener("click", back);
+
+  function select(i) {
+    var x = getKey(this);
+    if (x == (chars.length + 1)) {
+      //Virulence
+      window.open("175/5/index.html")
+    } else if (x == (chars.length + 3)) {
+      //R'eden
+      window.open("https://mmmegh.itch.io/reden")
+    } else if (x == (chars.length + 5)) {
+      //Balance
+      window.open("balance/balance.html")
+    } else if (x == (chars.length + 6)) {
+      //Valley
+      window.open("https://hub.link/QuVbfyT")
+    } else if (x == (chars.length + 8)) {
+      //Fragments
+      window.open("https://julsjacobson.itch.io/fragments")
+    } else if (x == (chars.length + 9)) {
+      //Hide and Sniff
+      window.open("https://julsjacobson.itch.io/hide-and-sniff")
+    } else {
+      modals[x].style.display = "block";
+      close.style.display = "block";
+      navbar.style.display = "none";
+    }
+  }
+
+  function playPreview(i) {
+    var x = getKey(this);
+    if (x >= chars.length) {
+      thumbs[x].play();
+    }
+  }
+
+  function pausePreview(i) {
+    var x = getKey(this);
+    if (x >= chars.length) {
+      thumbs[x].pause();
+    }
+  }
+
+  function getKey(val) {
+    return Object.keys(thumbs).find(key => thumbs[key] === val);
+  }
+
+  function back() {
+    for (i = 0; i < thumbs.length; i++) {
+      modals[i].style.display = "none";
+      close.style.display = "none";
+      navbar.style.display = "block";
+    }
+  }
+
+
+
+
 }
